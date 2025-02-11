624f5ff9f90108406052b789b0b1f7d25bc1170e
Aketchi-ux <mba49068@stud.hs-furtwangen.de>
2025-02-11 20:35:07 +0100

    Update Infos

--- main.ts
+++ main.ts
@@ -12,7 +12,7 @@ namespace Main {
     window.addEventListener("load", async () => {
         let canvas = document.getElementById("fullscreenCanvas") as HTMLCanvasElement;
         let ctx = canvas.getContext("2d")!; //(context) => crc2 is referring to ctx, used to draw on canvas
-        
+
         function resizeCanvas(): void {
             canvas.width = window.innerWidth;
             canvas.height = window.innerHeight;
@@ -32,22 +32,30 @@ namespace Main {
         // deleteQuery.set("collection", "Fireworks");
         // await fetch(url + "?" + deleteQuery.toString());
 
-        let createQuery: URLSearchParams = new URLSearchParams();
-        createQuery.set("command", "create");
-        createQuery.set("collection", "Fireworks");
-        await fetch(url + "?" + createQuery.toString());
+        let createQuery: URLSearchParams = new URLSearchParams();// creates a query string to send to the server
+        createQuery.set("command", "create");//request the server to create something
+        createQuery.set("collection", "Fireworks");//specifies the collection name
+        await fetch(url + "?" + createQuery.toString());//sends request to the server
+        //await ensures the request completes bevor continuing
 
         
         let savedQuery: URLSearchParams = new URLSearchParams();
-        savedQuery.set("command", "find");
+        savedQuery.set("command", "find");//request the server to find data
         savedQuery.set("collection", "Fireworks");
         savedQuery.set("data", "{}");
         let response = await fetch(url + "?" + savedQuery.toString());
+        //(savedQuery.toString) converts savedQuery (a URLSearchParams object) into a query string
+        //fetch sends a HTTP request to the server with this query
+        //await ensures the respons is fully received bevor moving to the next step
+        //response stores the servers reply
         let responseText = await response.text();
+        //await response.text() converts the servers response into a text string
+        //responsetext now contains the raw JSON data from the server
         let responseData = JSON.parse(responseText).data;
+        //JSON.parse(responseText) converts the JSON text into a JavaScript object
+        //.data extracts the actual firework data from the response
 
-        for(let itemId in responseData) 
-        {
+        for(let itemId in responseData) {
             createFireworksButton(itemId);
         }
     })
@@ -104,26 +112,31 @@ namespace Main {
         buttonCounter++;
         // When clicking the saved button, apply its settings
         saveing.addEventListener("click", async () => {
-            let buttonId = saveing.getAttribute("id")!;
-            let query: URLSearchParams = new URLSearchParams();
-            query.set("command", "find");
-            query.set("collection", "Fireworks");
-            query.set("id", buttonId);
-            let response = await fetch(url + "?" + query.toString());
+            let buttonId = saveing.getAttribute("id")!;//gets the id of the clicked button, which was assigned when the firework was saved
+            let query: URLSearchParams = new URLSearchParams();//creates URL parameters (query) to send a request to the server
+            query.set("command", "find");//informs the server we want to find data
+            query.set("collection", "Fireworks");//specifies which database collection to look in
+            query.set("id", buttonId);//requests the specific firework based on its ID
+            let response = await fetch(url + "?" + query.toString());//send an HTTP request to the server, converts the parameters into a Url query string
             let responseJson = response.ok ? await response.text() : "{}"; //If Ok true, response.text, if false {}
             let savedData = JSON.parse(responseJson).data[buttonId];
+            // converts hte response (JSON string) into an object
+            //extracts the specific fireworks data using buttonId
     
-            if (!savedData) 
-            {
+            if (!savedData) {
                 return;
             }
+            //if the savedData is empty or undefined, it exits (return), avoid errors
+
             Firework.explosionSizeSlider.value = savedData.explosionSize;
             particleSlider.value = savedData.particleCount;
             color.selectedColor = savedData.fireworkColor;
             (document.getElementById("fireworkcolor") as HTMLInputElement).value = savedData.fireworkColor;
-    
+            //updates explosion size slider, particle slider, color
+
             Firework.explosionSizeValue.textContent = Firework.explosionSizeSlider.value;
             particleCountValue.textContent = particleSlider.value;
+            //updates the numbers next to the slider to match the loaded firework
         
         });
 
